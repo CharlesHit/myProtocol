de81a74c38524690e5d191847d34c3ca635a3045
Charles He <mhe86@uwo.ca>
2019-11-22 22:02:55 -0500

    Final Version
    Introduced the delay, corrupt and package lost errorss

--- UDP_Client.py
+++ UDP_Client.py
@@ -2,178 +2,222 @@ import socket
 import struct
 import hashlib
 import timeit
-import signal
-import time
 
+#####
+# author Charles.M.H.
+# 250995508
+# Nov 20, 2019
+#####
+
+# address and port
 UDP_IP = "127.0.0.1"
 UDP_PORT = 5005
 
+# time delay 9 ms
 DELAY = 0.009
 
 
+# Using signal handler is definitely a better idea but I dropped it
+# For the sake of cross-platform
 def signal_handler(signum, frame):
     raise Exception("Timed out!")
 
 
-# organize data
-def rdt_builder(ack, seq, msg):
+# By the first three data, calculate the check sum then send it
+def rdt_builder(acknowledgement, sequence, message):
     # Build the UDP Packet
-    values = (ack, seq, msg)
+    values = (acknowledgement, sequence, message)
+    #   by some format
     data_struct = struct.Struct('I I 8s')
     packed_data = data_struct.pack(*values)
+    #   then calculate the check sum
     chksum = bytes(hashlib.md5(packed_data).hexdigest(), encoding="UTF-8")
-    values = (ack, seq, msg, chksum)
+    values = (acknowledgement, sequence, message, chksum)
+    # Build the final one
     pkt_struct = struct.Struct('I I 8s 32s')
     result_pkt = pkt_struct.pack(*values)
+    # send it!
     return result_pkt
 
 
+# check if a packet (already unpacked in tuple) has a ack == bit
 def is_ack(pkt, bit):
     return pkt[0] == bit
 
 
+# check if a packet (already unpacked in tuple) has a current check sum
 def corrupt(pkt):
-    #unpacker = struct.Struct('I I 8s 32s')
-    # unpacked_pkt = unpacker.unpack(pkt)
-    # Create the Checksum for comparison
+    # Build the UDP Packet and calculate the check sum
     values = (pkt[0], pkt[1], pkt[2])
     packer = struct.Struct('I I 8s')
     packed_data = packer.pack(*values)
     chksum = bytes(hashlib.md5(packed_data).hexdigest(), encoding="UTF-8")
-    # Compare chksum to test for corrupt data
+    # Compare check sums
     if pkt[3] == chksum:
         return False
     else:
         return True
 
 
-# Send Data
+# Send Data to the IP
 def rdt_send(send_pkt):
     sock.sendto(send_pkt, (UDP_IP, UDP_PORT))
 
 
-# Receive Data
+# Receive Data, with time check
+#   Actually it is not a good idea, because I didn't really check the running time
+#   But some package like timeit or signal, is platform bounded
+#   So, I have to...
 def rdt_receiver():
     # signal.signal(signal.SIGALRM, signal_handler)
     # signal.alarm(DELAY*1000)  # 9 mseconds
 
+    # get the time receive a data
     t_start = timeit.default_timer()
-
     rcv_pkt = sock.recvfrom(1024)
-
     t_end = timeit.default_timer()
 
+    # if time out, then throw an error
     if t_end - t_start > DELAY or rcv_pkt is None:
         raise Exception("Timed out!")
 
+    # unpack the data and return it
     unpacker = struct.Struct('I I 8s 32s')
     data, addr = rcv_pkt  # buffer size is 1024 bytes
     received_pack = unpacker.unpack(data)
     return received_pack
 
 
+# A toString printer
 def print_pkt(pkt):
     print("ACK " + str(pkt[0]) + ";  SEQ " + str(pkt[1]) + ";  DATA " + str(pkt[2]) + ";  CHKSUM " + str(pkt[3]))
 
 
-def output(pkt, sent_pkt, start_time, timer_opened):
-    # Received Packet (with all packet values shown)
-    print("====================")
-    print("Received: ", end='')
-    print_pkt(pkt)
-    # Packet Checksum Compare Result (ie. Corrupt or not corrupt)
-    if corrupt(rcvpkt):
-        print("corrupt")
-    else:
-        print("not corrupt")
-    # Sent Packet (with all packet values shown)
-    print("Sent Package: ", end='')
-    print_pkt(sent_pkt)
-    # Timer Expired
-    end_time = timeit.default_timer()
-    if start_time - end_time > DELAY and timer_opened is True:
-        print("time expired")
-    else:
-        print("time not expired")
-    print("====================")
-
+########################################################
+# main function
+########################################################
 
 print("UDP target IP:", UDP_IP)
 print("UDP target port:", UDP_PORT)
 
-# Send the UDP Packet
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # Internet, UDP
 
-msg_to_be_sent = [b'NCC-1701', b'NCC-1422', b'NCC-1017']
+# the message queue to be sent
+msg_queue = [b'NCC-1701', b'NCC-1422', b'NCC-1017']
 
-for message in msg_to_be_sent:
-    # print("New Turn")
-    # 0.0
-    sndpkt = rdt_builder(0, 0, message)
-    snd_tuple = (0, 0, message, None)
+for message in msg_queue:
+    # print("---->> New Turn:" + str(message))
+    print("================================")
+    # by default, the package will have seq = 0 in the beginning
+    seq = 0
+    #    ...also ack = 0 by default
+    sent_packet = rdt_builder(0, seq, message)
+    sent_tuple = (0, seq, message, None)  # this tuple is just a duplicated packet for printing info
+    rdt_send(sent_packet)  # send it
 
-    rdt_send(sndpkt)
-    # print("sent, 1")
+    # print the sent message
+    print("1 - Sent Package to server:\n    ", end="")
+    print_pkt(sent_tuple)
 
-    start = timeit.default_timer()
-    timer = True
+    # received packet is the one from the server
+    received_packet = None
+    while received_packet is None:
+        # Here the try statement is combined with the error throwing of rdt_receiver()
+        try:
+            received_packet = rdt_receiver()
+            # if we received something, and not time out,
+            #   and it is not corrupt, and the ack is correct,
+            #   we will go to the next handshake
+            if received_packet is not None and (corrupt(received_packet) is False and is_ack(received_packet, 0)):
+                timer = False
+            # Failed Case.
+            # Either Data is corrupted,
+            # or we received an empty package,
+            #     which means the package is lost
+            #
+            # We have to Keep Waiting data from the Server
+            #
+            # Notice, if this error happened, we need to keep sending.
+            # We must Stop this message
+            if received_packet is not None and (corrupt(received_packet) or is_ack(received_packet, 1)):
+                print("2 - Illegal msg received:\n    ", end="")
+                print_pkt(received_packet)
+                if corrupt(received_packet):
+                    if received_packet[2] == b'\x00' * 8 and received_packet[3] == b'\x00' * 32 and received_packet[0] == received_packet[1] == 0:
+                        print("WARNING: Lost ACK")
+                    else:
+                        print("WARNING: ACK corrupt")
+                else:
+                    print("  - ACK error")
+                # all this three case, you need to resend
+                print("================================")
+                rdt_send(sent_packet)
+                print("1 - Resent Package to server again:\n    ", end="")
+                print_pkt(sent_tuple)
+                received_packet = None
+            # Success case. Print what we get, and move on
+            if received_packet is not None:
+                print("2 - Package Received from Server.\n    ", end="")
+                print_pkt(received_packet)
+        # Failed Case of Time Out. Keep Receiving
+        except Exception:
+            rdt_send(sent_packet)
+            print("WARNING: Time Expired when wait for the server")
+            print("================================")
+            print("1 - Resent Package to server again:\n    ", end="")
+            print_pkt(sent_tuple)
+            received_packet = None
 
-    # 0.1
-    # print("0.1")
+    # if received_packet is set as true, then we skip this message
+    if received_packet is True:
+        continue
 
-    try:
-        rcvpkt = rdt_receiver()
-        # 0.2
-        if rcvpkt is not None and (corrupt(rcvpkt) or is_ack(rcvpkt, 1)):
-            print("illegal msg: " + str(message), end="")
-            if(corrupt(rcvpkt)):
-                print_pkt(": corrupt")
-            else:
-                print_pkt(": ack error")
-            # output(rcvpkt, snd_tuple, start, timer)
-        if rcvpkt is not None and (corrupt(rcvpkt) is False and is_ack(rcvpkt, 0)):
-            timer = False
-        if rcvpkt is not None:
-            print("Package Received from Server. ", end="")
-            print_pkt(rcvpkt)
-    except Exception:
-        if timer is True:
-            rdt_send(sndpkt)
-            print("Time Expired, resent: " + str(message))
-            start = timeit.default_timer()
-            continue
+    # Next step: the third and forth hand shaking
+    # In the Client side, it is our duty
+    #   to update the ACK, mode by 2
+    # and keep seq as it is
+    ack = (received_packet[0] + 1) % 2
+    seq = received_packet[1]
+    sent_packet = rdt_builder(ack, seq, message)
+    sent_tuple = (ack, seq, message, received_packet[3])
+    rdt_send(sent_packet)
+    print("3 - Sent Package to server:\n    ", end="")
+    print_pkt(sent_tuple)
 
-    # 1.0
-    # print("1.0")
-    sndpkt = rdt_builder(0, 1, message)
-    snd_tuple = (0, 1, message, rcvpkt[3])
-    rdt_send(sndpkt)
-    # print("sent, 2")
-
-    start = timeit.default_timer()
-    timer = True
-
-    # 1.1
-    # print("1.1")
-    stop = timeit.default_timer()
-    try:
-        rcvpkt = rdt_receiver()
-        # 0.2
-        if rcvpkt is not None and (corrupt(rcvpkt) or is_ack(rcvpkt, 0)):
-            # output(rcvpkt, snd_tuple, start, timer)
-            print("illegal msg: " + str(message), end="")
-            if (corrupt(rcvpkt)):
-                print_pkt(": corrupt")
-            else:
-                print_pkt(": ack error")
-        if rcvpkt is not None and (corrupt(rcvpkt) is False and is_ack(rcvpkt, 1)):
-            timer = False
-        if rcvpkt is not None:
-            # output(rcvpkt, snd_tuple, start, timer)
-            print("Package Received from Server. ", end="")
-            print_pkt(rcvpkt)
-    except Exception:
-        if timer is True:
-            print("Time Expired, resent: " + str(message))
-            rdt_send(sndpkt)
-            start = timeit.default_timer()
+    # Mirror Story above
+    received_packet = None
+    while received_packet is None:
+        try:
+            received_packet = rdt_receiver()
+            # Success Case
+            if received_packet is not None and (corrupt(received_packet) is False and is_ack(received_packet, 1)):
+                timer = False
+            # Failed Case. Corrupt. Keep Receiving
+            if received_packet is not None and (corrupt(received_packet) or is_ack(received_packet, 0)):
+                print(received_packet is not None)
+                print(is_ack(received_packet, 0))
+                print("4 - Illegal msg receive+d: " + str(message) + "\n    ", end="")
+                if corrupt(received_packet):
+                    if received_packet[2] == b'\x00' * 8 and received_packet[3] == b'\x00' * 32 and received_packet[0] == received_packet[1] == 0:
+                        print("WARNING: Lost ACK")
+                    else:
+                        print("WARNING: ACK corrupt")
+                else:
+                    print_pkt("WARNING: ACK error")
+                print("================================")
+                # all this three case, you need to resend
+                rdt_send(sent_packet)
+                print("1 - Resent Package to server again:\n    ", end="")
+                print_pkt(sent_tuple)
+                received_packet = None
+            if received_packet is not None:
+                print("4 - Package Received from Server.\n    ", end="")
+                print_pkt(received_packet)
+                print("Data Exchange Completed!")
+                print("================================")
+        # Failed Case. Time Out. Keep Receiving
+        except Exception:
+            rdt_send(sent_packet)
+            print("WARNING: Time Expired when wait for server")
+            print("3 - Resent Package to server again:\n    ", end="")
+            print_pkt(sent_tuple)
--- UDP_Server.py
+++ UDP_Server.py
@@ -4,22 +4,25 @@ import time
 import random
 import hashlib
 
+#####
+# author Charles.M.H.
+# 250995508
+# Nov 20, 2019
+#####
+
 UDP_IP = "127.0.0.1"
 UDP_PORT = 5005
 unpacker = struct.Struct('I I 8s 32s')
 
 
-# organize data
-def rdt_builder(ack, seq, pkt, check_sum):
+# By the first three data, calculate the check sum then send it
+#   Slight different by the Client's version
+def rdt_builder(acknowledgement, sequence, message, check_sum):
     # Build the UDP Packet
-    msg = pkt
-    # values = (ack, seq, msg)
-    # data_struct = struct.Struct('I I 8s')
-    # packed_pkt = data_struct.pack(*values)
-    # check_sum = bytes(hashlib.md5(packed_pkt).hexdigest(), encoding="UTF-8")
-    values = (ack, seq, msg, check_sum)
+    values = (acknowledgement, sequence, message, check_sum)
     pkt_struct = struct.Struct('I I 8s 32s')
     result_pkt = pkt_struct.pack(*values)
+    # send it!
     return result_pkt
 
 
@@ -35,40 +38,35 @@ def rdt_send(send_pkt, remote_address):
     sock.sendto(send_pkt, (UDP_IP, remote_address))
 
 
-def set_ack(pkt_value):
-    packed_data_arr = bytearray(pkt_value)
-    packed_data_arr[3] = 1
-    return bytes(packed_data_arr)
-
-
-def set_seq(pkt_value):
-    packed_data_arr = bytearray(pkt_value)
-    packed_data_arr[7] = 1
-    return bytes(packed_data_arr)
-
-
+# the function generate the transaction delay
 def network_delay():
-    if True and random.choice([0,1,0]) == 1:  # Set to False to disable Network Delay. Default is 33% packets are delayed
+    if True and random.choice(
+            [0, 1, 1]) == 1:  # Set to False to disable Network Delay. Default is 33% packets are delayed
         time.sleep(.01)
-        print("Packet Delayed")
+        print("    Packet Delayed")
+        return True
     else:
-        print("Packet Sent")
+        print("    Packet Sent")
+        return False
 
 
 def network_loss():
-    if True and random.choice([0,1,1,0]) == 1:  # Set to False to disable Network Loss. Default is 50% packets are lost
+    if True and random.choice(
+            [0, 1, 1, 0]) == 1:  # Set to False to disable Network Loss. Default is 50% packets are lost
         print("Packet Lost")
-        return 1
+        return True
     else:
-        return 0
+        return False
 
 
-def packet_checksum_corrupter(packetdata):
+def packet_checksum_corrupter():
     error_msg = b'Corrupt!'
-    if True and random.choice([0,1,0,1]) == 1:  # Set to False to disable Packet Corruption. Default is 50% packets are corrupt
-        return error_msg
+    if True and random.choice(
+            [0, 1, 0, 1]) == 1:  # Set to False to disable Packet Corruption. Default is 50% packets are corrupt
+        print("Data Corrupt")
+        return True
     else:
-        return packetdata
+        return False
 
 
 def get_check_sum(pkt):
@@ -89,51 +87,131 @@ def calculate_check_sum(ack, seq, msg):
     return check_sum
 
 
-
 # Create the socket and listen
 sock = socket.socket(socket.AF_INET,  # Internet
                      socket.SOCK_DGRAM)  # UDP
 sock.bind((UDP_IP, UDP_PORT))
 
 while True:
-    # 1
-    # Receive Data
-    data, addr = sock.recvfrom(1024)  # buffer size is 1024 bytes
-    UDP_Packet = unpacker.unpack(data)
-    print("1 - received from:", addr)
-    print("received message:", UDP_Packet)
+    # the indicator to control the artificial error
+    no_error_happened = True
+    while no_error_happened is True:
+        print("================================")
+        data, addr = sock.recvfrom(1024)  # buffer size is 1024 bytes
+        UDP_Packet = unpacker.unpack(data)
+        print("1 - received from:", addr)
+        print("    received message:", UDP_Packet)
+        # Compare check sums to test for corrupt data
+        chksum = get_check_sum(data)
+        # if it is corrupt
+        if UDP_Packet[3] == chksum and has_seq(UDP_Packet, 0):
+            print('1 - CheckSums matched, packet OK')
+            print("    1 - sent package back to client")
 
-    # Compare check sums to test for corrupt data
-    chksum = get_check_sum(data)
-    if UDP_Packet[3] == chksum and has_seq(UDP_Packet, 0):
-        print('CheckSums Match, Packet OK')
-        rdt_send(rdt_builder((UDP_Packet[0])%2, 0, UDP_Packet[2],
-                             calculate_check_sum((UDP_Packet[0])%2, 0, UDP_Packet[2])), addr[1])
-        network_delay()
-        print("sent 1-1")
-    else:
-        if UDP_Packet[3] != chksum or has_seq(UDP_Packet, 0):
-            print('CheckSums Do Not Match, Packet Corrupt')
-            rdt_send(rdt_builder((UDP_Packet[0]) % 2, 0, UDP_Packet[2],
-                                 calculate_check_sum((UDP_Packet[0]) % 2, 0, UDP_Packet[2])), addr[1])
-        print("sent 1-2")
+            ########################################
+            # Choose either Delays, Loss or Checksum_Corrupter
+            ########################################
 
-    # 2
+            ########################################
+            # 1. Delays
+            # if you wanna make a delay, comment the second line; otherwise the first
+            # 1
+            # no_error_happened = network_delay()
+            # 2
+            # no_error_happened = False
+            ########################################
+
+            ########################################
+            # 2. Loss
+            # if you wanna make a loss, set the part 3 all uncomment,
+            #   then comment the second line; otherwise comment the first
+            # 1
+            no_error_happened = network_loss()
+            # 2
+            # no_error_happened = False
+            #
+            # 3
+            if no_error_happened is True:
+                rdt_send(rdt_builder(0, 0, b'', b''), addr[1])
+                continue
+            ########################################
+
+            ########################################
+            # 3. checksum_corrupter
+            # if you wanna make a checksum corrupter, set the part 3 all uncomment,
+            #   then comment the second line; otherwise comment the first
+            # 1
+            no_error_happened = packet_checksum_corrupter()
+            # 2
+            # no_error_happened = False
+            # 3
+            if no_error_happened is True:
+                ERROR_SEQ = 512
+                ERROR_MSG = b'Corrupt!'
+                rdt_send(rdt_builder(UDP_Packet[0], UDP_Packet[1], UDP_Packet[2],
+                                     calculate_check_sum(UDP_Packet[0], ERROR_SEQ, ERROR_MSG)), addr[1])
+                continue
+            ########################################
+
+            # the sender will keep the ACK as it is
+            #   while change the SEQ
+            seq = (UDP_Packet[1] + 1) % 2
+            rdt_send(rdt_builder(UDP_Packet[0], seq, UDP_Packet[2],
+                                 calculate_check_sum(UDP_Packet[0], seq, UDP_Packet[2])), addr[1])
+        else:
+            # if it corrupt, there is two scenarios:
+            #
+            # 1. the check sum error
+            #
+            # 2. the sequential error
+            if UDP_Packet[3] != chksum or has_seq(UDP_Packet, 0):
+                print('1 - CheckSums failed to match, Packet Corrupt')
+                print("    2 - sent package back to client:", UDP_Packet)
+                seq = (UDP_Packet[1] + 1) % 2
+                rdt_send(rdt_builder(UDP_Packet[0], seq, UDP_Packet[2],
+                                     calculate_check_sum(UDP_Packet[0], seq, UDP_Packet[2])), addr[1])
+            else:
+                print("It is illegal to have the SEQ = %d.\nDropped" % (UDP_Packet[1]))
+
+                # keep in mind, in sequential error, we have to update both seq and ack
+                seq = (UDP_Packet[1] + 1) % 2
+                ERROR_SEQ = 100
+                ack = (UDP_Packet[0] + 1) % 2
+                rdt_send(rdt_builder(ack, seq, UDP_Packet[2],
+                                     calculate_check_sum(ack, ERROR_SEQ, UDP_Packet[2])), addr[1])
+                delayed = True
+
+    # 2 - almost the same. Only has_seq(UDP_Packet, 1) is different
     # Receive Data
     data, addr = sock.recvfrom(1024)  # buffer size is 1024 bytes
     UDP_Packet = unpacker.unpack(data)
     print("2 - received from:", addr)
-    print("received message:", UDP_Packet)
+    print("    received message:", UDP_Packet)
 
     # Compare check sums to test for corrupt data
     chksum = get_check_sum(data)
-    print(UDP_Packet)
     if UDP_Packet[3] == chksum and has_seq(UDP_Packet, 1):
         print('2 - CheckSums Match, Packet OK')
-        rdt_send(rdt_builder((UDP_Packet[0]+1)%2, 1, UDP_Packet[2], calculate_check_sum((UDP_Packet[0]+1)%2, 1, UDP_Packet[2])), addr[1])
-        print("sent 2-1")
+        # Sent
+        print("    2 - sent package back to client:", UDP_Packet)
+        # the sender will keep the ACK as it is while change the SEQ
+        seq = (UDP_Packet[1] + 1) % 2
+        rdt_send(rdt_builder(UDP_Packet[0], seq, UDP_Packet[2], calculate_check_sum(UDP_Packet[0], seq, UDP_Packet[2])),
+                 addr[1])
     else:
-        # if UDP_Packet[3] != chksum or has_seq(UDP_Packet, 1):
-        print('2 - CheckSums Do Not Match, Packet Corrupt')
-        rdt_send(rdt_builder((UDP_Packet[0]+1)%2, 1, UDP_Packet[2], calculate_check_sum((UDP_Packet[0]+1)%2, 1, UDP_Packet[2])), addr[1])
-        print("sent 2-2")
+        if UDP_Packet[3] != chksum or has_seq(UDP_Packet, 1):
+            # if UDP_Packet[3] != chksum or has_seq(UDP_Packet, 1):
+            print('2 - CheckSums Do Not Match, Packet Corrupt')
+            seq = (UDP_Packet[1] + 1) % 2
+            rdt_send(
+                rdt_builder(UDP_Packet[0], seq, UDP_Packet[2], calculate_check_sum(UDP_Packet[0], seq, UDP_Packet[2])),
+                addr[1])
+            print("    2 - sent package back to client")
+        else:
+            print("It is illegal to have the SEQ = %d.\nDropped" % (UDP_Packet[1]))
+            seq = (UDP_Packet[1] + 1) % 2
+            ERROR_SEQ = 100
+            ack = (UDP_Packet[0] + 1) % 2
+            rdt_send(rdt_builder(ack, seq, UDP_Packet[2],
+                                 calculate_check_sum(ack, ERROR_SEQ, UDP_Packet[2])), addr[1])
+            delayed = True
